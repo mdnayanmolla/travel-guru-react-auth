2bf02fd7aa664ab344c61bcec67286cc32d4b8ac
mdnayanmolla <mdnayanmollaedu@gmail.com>
2023-10-04 23:30:41 +0600

    first commit

---
+++ src/Route/Route.jsx
@@ -0,0 +1,10 @@
+import {createBrowserRouter} from "react-router-dom";
+
+const routes = createBrowserRouter([
+    {
+        path:'/',
+        element: <div>Hello</div>
+    }
+]);
+
+export default routes;
